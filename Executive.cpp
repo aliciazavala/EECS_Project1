a3bdc9711327374d64fa22a7721a62d8efa3bba4
Manuel Sandoval <m905s765@ku.edu>
2020-02-14 16:11:19 -0600

    Update Executive

--- Executive.cpp
+++ Executive.cpp
@@ -1,3 +1,64 @@
 #include "Executive.h"
 
+Executive::Executive()
+{
+	//both should be empty at startup
+	m_calendar = nullptr;
+	m_menuStack = nullptr;
+	
+	m_fileName = "data.txt"; //file name should be constant
+}
 
+Executive::~Executive()
+{
+	if(m_calendar != nullptr)
+	{
+		delete m_calendar;
+	}
+	if(m_menuStack != nullptr)
+	{
+		delete m_menuStack;
+	}
+}
+
+void Executive::run()
+{
+	if(load())//executes load
+	{
+		m_menuStack = new Stack<Menu>;
+		pushMenu("MainMenu");
+		
+	}
+	while(!m_menuStack->isEmpty())//loops as long as there are menus
+	{
+		
+	}
+	save();
+}
+
+bool Executive::load()
+{
+	//read from file then return true or return false if file cannot be openned
+
+	int eventsLoaded = 1;//should be read from file
+	m_calendar = new Calendar(eventsLoaded);
+
+	return true;//placeholder return
+}
+
+void Executive::save()
+{
+	//save to file
+	//possible feature: encrypt the save file to prevent it from being read or altered
+	//			ex: convert chars to bits (8 bits) and then use XOR encryption
+	//				   x XOR key = y, where x, y, and key are 8bit strings
+	//				=> y XOR key = x
+
+
+	std::cout << "save complete\n";//placeholder return
+}
+
+void Executive::pushMenu(std::string menuName)
+{
+	
+}
--- Executive.h
+++ Executive.h
@@ -3,17 +3,28 @@
 
 #include "Calendar.h"
 #include "Display.h"
+#include "Menu.h"
 #include "MainMenu.h"
 #include "MonthMenu.h"
+#include "StackInterface.h"
 
 
 class Executive
 {
 	public:
-	
+	Executive();
+	~Executive();
+
+	void run();
+
 	private:
-	Calendar* eventList;
-	Stack<Menu>* menuStack;
+	bool load();
+	void save();
+	void pushMenu(std::string menuName);
+
+	std::string m_fileName;
+	Calendar* m_calendar;
+	Stack<Menu>* m_menuStack;
 };
 
 #endif
