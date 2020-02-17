796347da6b21a3d3fd6a2579971401aba604927f
Alicia Zavala <alicia@ku.edu>
2020-02-17 17:20:35 -0600

    Add comments

--- save-load-testing/main.cpp
+++ save-load-testing/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
   char option;
-  string name,eName,time;
+  string name,eName,startT, endT;
   //writes events
   ofstream events;
   //reads events
@@ -20,30 +20,33 @@ int main()
     if(option == 'a')
       {
         //saving format:
-        // EVENT NAME     ADMIN NAME      EVENT TIME
+        // EVENT NAME     ADMIN NAME      EVENT START TIME      EVENT END TIME
         events.open("January.txt", fstream::app); //::app to keep writing on files
         cout<<"\nEnter event admin name: ";
         cin>>name;
         cout<<"\nEnter event name: ";
         cin>>eName;
-        cout<<"\nEnter event date and time: ";
-        cin>>time;
+        cout<<"\nEnter event start time (XX:XX): ";
+        cin>>startT;
+        cout<<"\nEnter event end time (XX:XX): ";
+        cin>>endT;
         cout<<"\nEvent saved\n";
-        events<<eName<<'\t'<<name<<'\t'<<time<<'\n';
+        events<<eName<<'\t'<<name<<'\t'<<startT<<'\t'<<endT<<'\n';
         events.close();
       }
     if(option == 'b')
       {
         //reading format:
-        // EVENT NAME     ADMIN NAME      EVENT TIME
+        // EVENT NAME     ADMIN NAME      EVENT START TIME      EVENT END TIME
         output.open("January.txt");
         int i = 1;
         while(output >> eName)
         {
           output>>name;
-          output>>time;
+          output>>startT;
+          output>>endT;
           cout<<"===========\nEVENT "<<i<<"\n===========\n";
-          cout<<"Name of the Event:" << eName<<"\nEvent admin: "<<name<<"\nEvent time and date: "<<time<<"\n\n";
+          cout<<"Name of the Event:" << eName<<"\nEvent admin: "<<name<<"\nEvent start time: "<<startT<<"\nEvent end time: "<<endT<<"\n\n";
           i++;
         }
         output.close();
