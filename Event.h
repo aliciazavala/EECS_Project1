d2d1b9e73a71daa0948ad9fa098bbb48bd49d301
Joseph <j310d455@ku.edu>
2020-02-18 19:22:29 -0600

    added and implemented setters and getters for ID member in event class

--- Event.h
+++ Event.h
@@ -12,12 +12,16 @@ class Event
 {
 	public:
 	Event();//this constructor should not be used
-	
+
 	std::string getMonth() const;
 	void setMonth(std::string month);
 
+	int getID() const;
+	void setID(int num);
+
 	private:
 	std::string m_month;
+	int m_ID;
 };
 
 #endif
