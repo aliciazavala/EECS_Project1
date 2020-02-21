54ae42f4596a4cc164e26c00a0470a741ab2a709
Alicia Zavala <aliciazafe@hotmail.com>
2020-02-21 15:42:24 -0600

    Update ViewEventMenu.cpp

--- ViewEventMenu.cpp
+++ ViewEventMenu.cpp
@@ -1,1 +1,16 @@
+#include "ViewEventMenu.h"
 
+ViewEventMenu::ViewEventMenu()
+{
+  m_menuName= "ViewEventMenu";
+}
+
+ViewEventMenu::~ViewEventMenu()
+{
+  
+}
+
+std::string ViewEventMenu::getName() const
+{
+	return m_menuName;
+}
