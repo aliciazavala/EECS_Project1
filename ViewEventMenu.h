a44faade4b7bbb7fcf291500df64f5126ed16c31
Alicia Zavala <aliciazafe@hotmail.com>
2020-02-21 15:38:32 -0600

    Update ViewEventMenu.h

--- ViewEventMenu.h
+++ ViewEventMenu.h
@@ -1,1 +1,18 @@
+#ifndef VIEWEVENTMENU_H
+#define VIEWEVENTMENU_H
+#include"Menu.h"
+#include "project1_lib.h"
+class ViewEventMenu: public Menu
+{
+public:
+  ViewEventMenu(int monthEvent);
+  ~ViewEventMenu();
+  void print() const;
 
+  std::string getName() const;
+
+private:
+std::string m_menuName;
+
+};
+#endif
