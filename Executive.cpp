709a14da549039fc53bc538c3f27e67642e19a96
Alicia Zavala <aliciazafe@hotmail.com>
2020-02-21 15:57:28 -0600

    Update Executive.cpp

--- Executive.cpp
+++ Executive.cpp
@@ -163,7 +163,7 @@ void Executive::handleMonthMenu()
 	else if(input>0 && input<range+1)
 	{
 		m_eventId = temp.returnID(input-1);
-		Menu* newMenu = new ViewEventMenu();
+		Menu* newMenu = new ViewEventMenu(m_eventId);
 		m_menuStack->push(newMenu);
 	}
 	
@@ -286,77 +286,17 @@ void Executive::handleSettingsMenu()
 }
 void Executive::handleViewEventMenu()
 {
-	std::ifstream fin;
-    std::string temp;
-    std::string creatorName;
-    std::string eventName;
-	std::string timeArray;
-    int eventId;
-    int month;
-    int day;
-    int year;
-    int userChoice;
-
-	fin.open("./data/" + nameOfMonth(m_loadedMonth)+".txt");//open file month
-    while(fin>>temp)
-    {
-		fin>>eventId;
-		std::getline(fin,eventName);
-		std::getline(fin,eventName);
-		fin>>month;
-		fin>>day;
-		fin>>year;
-		std::getline(fin,creatorName);
-		std::getline(fin,creatorName);
-		std::getline(fin,timeArray);
-
-		if (eventId == m_eventId)
-		{
-			std::cout << "\t ===== " << eventName << " =====" << std::endl;
-			//USE LIBRARY TO ALSO GET DAY OF THE WEEK??
-			std::cout<<"Date: "<< nameOfMonth(m_loadedMonth) <<", "<< dayOfWeek(m_loadedMonth,day,m_loadedYear) << " "<<day<<" "<< m_loadedYear << "\n" ;
-			std::cout<<"Event Creator: "<<creatorName<<"\n";
-			std::cout<<"Attendees: "<<"\n";
-			//READ ATTENDEES
-			std::ifstream attendees;
-			std::string attendeeName;
-			int id;
-
-			attendees.open("./data/Attendees.txt");
-			while(attendees>>id)
-			{
-			if(id == m_eventId)
-			{
-			std::getline(attendees,attendeeName);
-			std::getline(attendees,timeArray);
-
-			std::cout<<attendeeName<<std::endl;
-			//Line missing: print attending time
-			}
-
-
-			//gets attendee's name line and attendee's time line and skips them
-			else
-			{
-			std::getline(attendees,attendeeName);
-			std::getline(attendees,timeArray);
-			}
-
-			}
-
-			std::cout<<"\n";
-			attendees.close();
-			
-		}
-				
-	}
-
-	std::cout << "[1] Attend" << std::endl;
-	std::cout << "[0] Back" << std::endl;
+	ViewEventMenu temp = ViewEventMenu(m_eventId);
+	
 	userChoice=getIntRangeFromUser(0,1);
+	
 	if(userChoice==0)
 	{
 		handleBack();
+	}
+	if(userChoice==1)
+	{
+		//attend menu
 	}
 
 }
