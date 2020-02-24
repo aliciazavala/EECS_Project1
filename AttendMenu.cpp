b88465a6b4fdf9143df6c358bfbd35be6dfc693d
Alicia Zavala <alicia@ku.edu>
2020-02-24 08:58:26 -0600

    Edit

--- AttendMenu.cpp
+++ AttendMenu.cpp
@@ -14,7 +14,7 @@ void AttendMenu::print()
 {
 	std::string name;
 	std::ofstream attendees;
-	attendees.open("./data/Attendees.txt");
+	attendees.open("./data/Attendees.txt",std::fstream::app);
 	clearScreen();
 	std::cout << "\t ===== Attending Event =====" << std::endl;
 	std::cout << " Enter name: ";
