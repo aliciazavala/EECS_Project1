09d015b35fb7dade5b3316421e095d3c7bbd9bde
Alicia Zavala <alicia@ku.edu>
2020-02-24 08:57:04 -0600

    Add files via upload

--- AttendMenu.cpp
+++ AttendMenu.cpp
@@ -1,1 +1,36 @@
+#include "AttebdMenu.h"
 
+AttendMenu::AttendMenu(int id)
+{
+	m_menuName = ;
+	m_ID = id;
+}
+AttendMenu::AttendMenu(){}
+
+
+AttendMenu::~AttendMenu(){}
+
+void AttendMenu::print()
+{
+	std::string name;
+	std::ofstream attendees;
+	attendees.open("./data/Attendees.txt",std::fstream::app);
+	clearScreen();
+	std::cout << "\t ===== Attending Event =====" << std::endl;
+	std::cout << " Enter name: ";
+	std::cin >> name;
+	attendees<<m_ID<<" "<<name<<std::endl;
+}
+
+void print()const{}
+
+std::string AttendMenu::getName() const
+{
+	return (m_menuName);
+}
+
+    string m_name;
+
+
+};
+#endif
--- AttendMenu.h
+++ AttendMenu.h
@@ -1,1 +1,27 @@
+#ifndef ATTENDMENU_H
+#define ATTENDMENU_H
 
+#include "Menu.h"
+#include "project1_lib.h"
+#include <fstream>
+#include <string.h>
+
+
+class AttendMenu : public Menu
+{
+  public:
+    AttendMenu(int id);
+    AttendMenu();
+    ~AttendMenu();
+    void print();
+    void print()const;
+    std::string getName() const;
+
+  private:
+    std::string m_menuName;
+    int m_ID;
+    string m_name;
+
+
+};
+#endif
