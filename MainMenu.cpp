d5b2254e87f508d3c206155e53a917ec1e86e964
Joseph <j310d455@ku.edu>
2020-02-25 20:26:51 -0600

    commented on MainMenu::print() function

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -21,7 +21,7 @@ void MainMenu::print()const
 }
 
 
-void MainMenu::print(int year) const
+void MainMenu::print(int year) const //this function displays the main menu of the program in a formatted way.
 {
 	clearScreen();
 	std::cout << "\t===== "<< year << " CALENDAR =====\n";
@@ -29,14 +29,14 @@ void MainMenu::print(int year) const
 	std::cout << "\tChose a month:\n";
 	for (int i = 1; i <= 12; i++)
 	{
-		std::cout << "[" << i << "] " << nameOfMonth(i);
+		std::cout << "[" << i << "] " << nameOfMonth(i); //prints the name of the month with the corresponding number by it
 		if(i == 1)
 		{
-			std::cout << "\t\t[s] Settings";
+			std::cout << "\t\t[s] Settings"; //prints settings option on same line as january
 		}
 		else if(i == 2)
 		{
-			std::cout << "\t\t[q] Quit";
+			std::cout << "\t\t[q] Quit"; //prints quit option on same line as february
 		}
 		std::cout << std::endl;
 	}
